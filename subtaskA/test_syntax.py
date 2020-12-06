0cb2f7a7e822151844e7df12628c4c1c3756a9a5
Zafar Mahmood <zfar.mah@gmail.com>
2020-12-06 20:40:01 +0100

    new commit

---
+++ subtaskA/test_syntax.py
@@ -0,0 +1,9 @@
+
+
+( {"Gabby": 8 , "Maelle": 5}, 
+{"Gabby": 888 , "Maelle": 599},
+{"Gabby": 123 , "Maelle": 46}
+)
+
+print(sum(tuple(( {"Gabby": 8 , "Maelle": 5},  {"Gabby": 888 , "Maelle": 599},{"Gabby": 123 , "Maelle": 46}
+))))
